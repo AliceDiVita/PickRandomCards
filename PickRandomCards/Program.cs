1b4a18e4251918997c3d9347c57e732016fba5cb
AliceDiVita <alicedivita9@gmail.com>
2023-03-01 11:30:34 +0100

    Aggiungere i file di progetto.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCards;
+
+Console.Write("Enter the numbers of cards to pick ");
+string line = Console.ReadLine();
+
+if (int.TryParse(line, out int numberOfCards))
+{
+    string[] arrayOfCards = CardPicker.PickSomeCards(numberOfCards);
+	foreach (string card in arrayOfCards)
+	{
+		Console.WriteLine(card);
+	}
+}
+else
+{
+    Console.WriteLine("The numer submitted is not valid");
+}
